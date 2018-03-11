aeeabfa7ffc0add206c02e54f8a1919fb83f36d5
blackopss <36933564+blackopss@users.noreply.github.com>
2018-03-11 17:42:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -107,31 +107,31 @@ message.delete()
 
 
 client.on('message', function(message) {
-if(!message.channel.guild) return;
-if(message.content === prefix + 'colors create') {
-if(message.member.hasPermission('MANAGE_ROLES')) {
-setInterval(function(){})
-message.channel.send('يتم إنشاء ٢٠٠ لون إنتظر | ▶️')
-}else{
-message.channel.send('**⚠ | لا يوجد لديك صلاحية**')
-}
-}
+                  if(!message.channel.guild) return;
+    if(message.content ===  '.colors create') {
+        if(message.member.hasPermission('MANAGE_ROLES')) {
+            setInterval(function(){})
+            message.channel.send('جاري عمل الالوان |✅')
+        }else{
+            message.channel.send('ما معاك البرمشن المطلوب  |❌')
+            }
+    }
 });
 
 client.on('message', message=>{
-if (message.content === prefix + 'colors create'){
-if(!message.channel.guild) return;
-if (message.member.hasPermission('MANAGE_ROLES')){
-setInterval(function(){})
-let count = 0;
-let ecount = 0;
-for(let x = 1; x < 200; x++){
-message.guild.createRole({name:x,
-color: 'RANDOM'})
-}
-}
-}
-}); 
+    if (message.content ===  'colors create'){
+              if(!message.channel.guild) return;
+            if (message.member.hasPermission('MANAGE_ROLES')){
+                setInterval(function(){})
+                  let count = 0;
+                  let ecount = 0;
+        for(let x = 1; x < 141; x++){
+            message.guild.createRole({name:x,
+            color: 'RANDOM'})
+            }
+            }
+    }
+});
 
 
 
