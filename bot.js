2c0a29cc033ef8c48cc9410f27e12b302730fdaa
abadyxx <gamersspeaks@gmail.com>
2018-03-12 00:26:45 +0300

    test

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs');
 const prefix = ".";
-const sql = require("sqlite");
-sql.open("./credits.sqlite");
 
 
 client.on('ready', () => {
@@ -26,46 +24,6 @@ client.on('message', message => {
    if (message.content === prefix + "roll") {
   message.channel.sendMessage(Math.floor(Math.random() * 100));
     }
-});
-//// Credtis ////
-client.on("message", message => {
-  if (message.author.bot) return;
-  if (message.channel.type !== "text") return;
-
-  sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-    if (!row) {
-      sql.run("INSERT INTO scores (userId, credits,) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
-    } else {
-      let curLevel = Math.floor(0.1 * Math.sqrt(row.points + 1));
-      if (curLevel > row.level) {
-        row.level = curLevel;
-        sql.run(`UPDATE scores SET points = ${row.points + 1}, level = ${row.level} WHERE userId = ${message.author.id}`);
-        message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
-      }
-      sql.run(`UPDATE scores SET points = ${row.points + 1} WHERE userId = ${message.author.id}`);
-    }
-  }).catch(() => {
-    console.error;
-    sql.run("CREATE TABLE IF NOT EXISTS scores (userId TEXT, points INTEGER, level INTEGER)").then(() => {
-      sql.run("INSERT INTO scores (userId, points, level) VALUES (?, ?, ?)", [message.author.id, 1, 0]);
-    });
-  });
-
-  if (!message.content.startsWith(prefix)) return;
-
-  if (message.content.startsWith(prefix + "level")) {
-    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-      if (!row) return message.reply("**Your current level is 0**");
-      message.reply(`Your current level is ${row.level}`);
-    });
-  } else
-
-  if (message.content.startsWith(prefix + "credit")) {
-    sql.get(`SELECT * FROM scores WHERE userId ="${message.author.id}"`).then(row => {
-      if (!row) return message.reply('**Sorry your :credit_card: balance is $0.**');
-      message.reply( 'Your Balance is ' +  `${row.points}` + '$' + ' 💳');
-    });
-  }
 });
 
 /////members count////////
