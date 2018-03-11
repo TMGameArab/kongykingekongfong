347f5d9bdbf1d2cd3335c392fe382b038ef970c0
blackopss <36933564+blackopss@users.noreply.github.com>
2018-03-11 16:00:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1197,31 +1197,3 @@ if (message.content.startsWith(prefix + 'st')) {
 
 
 
-if (message.content.startsWith(".contact")) {
-if (!message.channel.guild) return;
-
-
-
-let args = message.content.split(" ").slice(1).join(' ');
-
-
-
-client.users.get("389755149939113987").send(
-    "\n" + "**" + "● السيرفر :" + "**" +
-    "\n" + "**" + "» " + message.guild.name + "**" +
-    "\n" + "**" + " ● المرسل : " + "**" +
-    "\n" + "**" + "» " + message.author.tag + "**" +
-    "\n" + "**" + " ● الرسالة : " + "**" +
-    "\n" + "**" + args + "**")
-
-let embed = new Discord.RichEmbed()
-                                                .setAuthor(message.author.username, message.author.avatarURL)
-                                                .setDescription(':mailbox_with_mail: تم ارسال الرسالة الى صاحب البوت بنجاح')
-                                                .setThumbnail(message.author.avatarURL)
-                                                .setFooter(message.author.username, message.author.avatarURL)
-                                                message.channel.sendEmbed(embed);
-
-
-}
-    
-});
