668a0d011ff06611fe22c131e1e7a91ea3645404
abadyxx <gamersspeaks@gmail.com>
2018-03-11 17:25:46 +0300

    ff

--- bot.js
+++ bot.js
@@ -1232,8 +1232,3 @@ client.on('message', message => {
       "\n" + "**" + args + "**")
   }
   });
-
-
-client.on('guildBanAdd', (guild, user) => {
-guild.defaultChannel.send(`${user.username} has been banned perma!`)
-})
