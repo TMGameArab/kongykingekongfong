8b5163777a28e7fe8232d1eb6c191a081d2b0e91
abadyxx <gamersspeaks@gmail.com>
2018-03-12 00:25:30 +0300

    test

--- bot.js
+++ bot.js
@@ -1293,10 +1293,24 @@ client.on('message', message => {
 
 var memes =["http://www.shuuf.com/shof/uploads/2015/09/09/jpg/shof_b9d73150f90a594.jpg","https://haltaalam.info/wp-content/uploads/2015/05/0.208.png","https://haltaalam.info/wp-content/uploads/2015/05/266.png","https://haltaalam.info/wp-content/uploads/2015/05/250.png","https://haltaalam.info/wp-content/uploads/2017/02/0.2517.png","https://pbs.twimg.com/media/CP0mi02UAAA3U2z.png","http://www.shuuf.com/shof/uploads/2015/08/31/jpg/shof_3b74fa7295ec445.jpg","http://www.shuuf.com/shof/uploads/2015/08/22/jpg/shof_fa3be6ab68fb415.jpg","https://pbs.twimg.com/media/CSWPvmRUcAAeZbt.png","https://pbs.twimg.com/media/B18VworIcAIMGsE.png"]
 client.on('message', message => {
-    var args = message.content.split(" ").slice(1);         
 if(message.content.startsWith(prefix + 'هل تعلم')) {
      var embed = new Discord.RichEmbed()
 .setImage(memes[Math.floor(Math.random() * memes.length)])
 message.channel.sendEmbed(embed);
 }
 });
+
+
+client.on('message', message => {
+  if(message.content === prefix + "إقتراح") {
+    if(message.author.bot) return;
+   
+    let args1 = message.content.split(' ').slice(1).join(' ')
+
+    let embed = new Discord.RichEmbed ()
+    .addField("الأسم:", message.author.username)
+    .addField("الأقتراح", args1);
+    message.channel.sendEmbed(embed);
+
+  }
+})
