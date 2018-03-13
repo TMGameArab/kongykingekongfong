7f3b5587382d4b9080060764c83a09a048cbf948
abadyxx <gamersspeaks@gmail.com>
2018-03-13 18:21:55 +0300

    go pls

--- bot.js
+++ bot.js
@@ -67,25 +67,6 @@ var imgxx = message.author
 }
 });
 
-
-client.on('message', message => {
-  if (message.content.startsWith(".avatar")) {
-      var mentionned = message.mentions.users.first();
-  var x5bzm;
-    if(mentionned){
-        var x5bzm = mentionned;
-    } else {
-        var x5bzm = message.author;
-        
-    }
-      const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setImage(`${x5bzm.avatarURL}`)
-    message.channel.sendEmbed(embed);
-  }
-});
-
-
 ///Anti-Advertising
 client.on('message', message => {
   if(message.content.includes("discord.gg")){
