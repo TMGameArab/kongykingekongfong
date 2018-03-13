a0ca474e6befc9c279f8f53c195804852d421238
abadyxx <gamersspeaks@gmail.com>
2018-03-13 18:29:56 +0300

    test

--- bot.js
+++ bot.js
@@ -63,8 +63,25 @@ client.on('message', message => {
 var imgxx = message.author;
     }
     
- message.channel.sendMessage(imgxx.avatarURL);
+ message.channel.sendMessage(`${imgxx.avatarURL}`);
 }
+});
+
+client.on('message', message => {
+    if (message.content.startsWith(".avatar")) {
+        var mentionned = message.mentions.users.first();
+    var x5bzm;
+      if(mentionned){
+          var x5bzm = mentionned;
+      } else {
+          var x5bzm = message.author;
+          
+      }
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .setImage(`${x5bzm.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
 });
 
 
