52a81031c0ad541d57f9e3ed5a4fbbace841df4c
abadyxx <gamersspeaks@gmail.com>
2018-03-15 16:49:29 +0300

    ff

--- bot.js
+++ bot.js
@@ -3,9 +3,6 @@ const client = new Discord.Client();
 const fs = require('fs');
 const Music = require('discord.js-musicbot-addon');
 const prefix = ".";
-const sql = require("sqlite");
-sql.open("./credits.sqlite", { cached: true });
-
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
@@ -1321,7 +1318,6 @@ const music = new Music(client, {
   ////////////////////////////////////////////////////////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////////////////////
   ///////////////////////////////////////////////////////////////////////////////////////////////
-
 var memes =["https://cdn.discordapp.com/attachments/419823645280436224/423563689954902016/unknown.png","https://cdn.discordapp.com/attachments/419823645280436224/423563233723547658/unknown.png","https://cdn.discordapp.com/attachments/419823645280436224/423562874850508804/unknown.png","https://cdn.discordapp.com/attachments/419823645280436224/423562665026125845/unknown.png","https://cdn.discordapp.com/attachments/419823645280436224/423562493726687253/unknown.png","https://cdn.discordapp.com/attachments/419823645280436224/423562319826518028/unknown.png","https://cdn.discordapp.com/attachments/419823645280436224/423562059167563778/unknown.png","https://cdn.discordapp.com/attachments/419823645280436224/423561747195101186/unknown.png"]
 client.on('message', message => {
     var args = message.content.split(" ").slice(1);         
