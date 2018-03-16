bdc1767d34d333d6f6547333f38130990985a4d0
Abady <gamersspeaks@gmail.com>
2018-03-16 20:48:07 +0300

    go!

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs');
 const config = require("./config.json");
+const prefix = config.prefix
 const Music = require('discord.js-musicbot-addon');
 client.login(process.env.BOT_TOKEN);
 ///////////////////////////////////////////////////////////////////////////
@@ -9,17 +10,6 @@ client.login(process.env.BOT_TOKEN);
 ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////
-
-client.on('message', message => {
-  let prefixes = JSON.parse(fs.readFileSync('./prefixes.json', 'utf8'));
-  if(!prefixes[message.guild.id]){
-    prefixes[message.guild.id] = {
-      prefixes: config.prefix
-    };
-  }
-});
-
-
 client.on('ready', () => {
     client.user.setActivity("Music!", {type: "LISTENING"});
       client.user.setStatus('KingBot Is Running');
