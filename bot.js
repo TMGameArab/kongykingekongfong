8e8c9c517632d19b589eda7b5252cdb38c891030
Abady <gamersspeaks@gmail.com>
2018-03-17 15:57:15 +0300

    kk

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs');
-const config = require("./config.json");
-const prefix = config.prefix;
+const prefix = ".";
 const Music = require('discord.js-musicbot-addon');
 client.login(process.env.BOT_TOKEN);
 ///////////////////////////////////////////////////////////////////////////
