610620501354d685dc1a5847cf880df956041e0f
Abady <gamersspeaks@gmail.com>
2018-03-22 15:28:27 +0300

    k

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('ready', ()  => {
 client.on('message', message => {
 if (message.content === '.globalbc' ){
     if(!message.channel.guild) return;
-  if(message.author.id !== devs) return;
+  if(message.author.id !== devs[devs.length]) return;
   let args = message.content.split(' ').slice(1).join(' ');
   message.channel.sendMessage(':incoming_envelope: ➤ **Sending to ' + `${client.users.size} users...**`);
   client.users.forEach(m =>{
@@ -26,7 +26,7 @@ if (message.content === '.globalbc' ){
 client.on('message', message => {
     var args = message.content.split(` `).slice(1);
     var argresult = args.join(` `);
-        if (message.author.id !== devs) return;
+        if (message.author.id !== devs[devs.length]) return;
   
   if (message.content === '.ply') {
     client.user.setGame(argresult);
@@ -309,20 +309,20 @@ message.author.sendEmbed(embed);
         if (!args[1]) {
     message.channel.send("**bc <message>**");
     return;
+    message.channel.send("**:loudspeaker: | يتم إرسال البرودكسات")
     }
             message.guild.members.forEach(m => {
                 var bc = new Discord.RichEmbed()
-            .setTitle('Broadcast From', `${message.guild.name}`)
                 .setThumbnail(message.guild.iconURL)
-                .addField('» Server :', `${message.guild.name}`)
-                .addField('» Sender: ', `${message.author.username}#${message.author.discriminator}`)
-                .addField(' » Message : ', args)
+                .setFooter(`» مرسلة من قبل: ${message.author.username}#${message.author.discriminator}`)
+                .addField(' » البرودكاست : ', args)
                 .setColor('RANDOM')
                 // m.send(`[${m}]`);
-                m.send(`${m}`,{embed: bc});
+                m.send({embed: bc});
             });
         }
         }
+
             if(message.content.startsWith(prefix + 'animal')) {
                 var cats = ["https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg","http://www.dogbazar.org/wp-content/uploads/2014/09/british-bull-dog-puppies.jpg","http://cdn2-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg","http://cdn.akc.org/Marketplace/Breeds/German_Shepherd_Dog_SERP.jpg","https://animalso.com/wp-content/uploads/2016/12/black-german-shepherd_2.jpg","https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpg","https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg","http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg","https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg","https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg","http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg","https://www.thelocal.de/userdata/images/article/fa6fd5014ccbd8f4392f716473ab6ff354f871505d9128820bbb0461cce1d645.jpg","https://www.adelaidezoo.com.au/wp-content/uploads/sites/2/animals/GiantPanda3Slider.jpg","http://imagem.band.com.br/f_230168.jpg"]
                    var cat = new Discord.RichEmbed()
