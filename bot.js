4cf5abdb47a200acfce293ff6c911ec826a2bc94
Abady <gamersspeaks@gmail.com>
2018-03-22 15:11:11 +0300

    ok

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const config = require("./config.json");
 const Discord = require("discord.js");
 const fs = require('fs');
 const client = new Discord.Client({disableEveryone: true});
-const devs = ['336606008069849088' || '315477177028050945' || '171259176029257728' || '343383616895713290']
+const devs = ['336606008069849088' || '315477177028050945' || '171259176029257728' || '343383616895713290'];
 const Music = require('discord.js-musicbot-addon');
 client.login(process.env.BOT_TOKEN);
 
@@ -301,26 +301,28 @@ message.author.sendEmbed(embed);
             if (!message) return message.reply('**ضع المينشان بشكل صحيح  ❌ **').catch(console.error);
         
         }
-        if(message.content == prefix + 'bc') {
-            if (message.author.id === client.user.id) return;
-            if (message.guild) {
-           let embed = new Discord.RichEmbed()
-            let args = message.content.split(' ').slice(1).join(' ');
-            if (!args[1]) {
-        message.channel.send("**bc <message>**");
-        return;
+        if(message.content.split(' ')[0] == prefix + 'bc') {
+        if (message.author.id === client.user.id) return;
+        if (message.guild) {
+       let embed = new Discord.RichEmbed()
+        let args = message.content.split(' ').slice(1).join(' ');
+        if (!args[1]) {
+    message.channel.send("**bc <message>**");
+    return;
+    }
+            message.guild.members.forEach(m => {
+                var bc = new Discord.RichEmbed()
+            .setTitle('Broadcast From', `${message.guild.name}`)
+                .setThumbnail(message.guild.iconURL)
+                .addField('» Server :', `${message.guild.name}`)
+                .addField('» Sender: ', `${message.author.username}#${message.author.discriminator}`)
+                .addField(' » Message : ', args)
+                .setColor('RANDOM')
+                // m.send(`[${m}]`);
+                m.send(`${m}`,{embed: bc});
+            });
         }
-                message.guild.members.forEach(m => {
-                    var bc = new Discord.RichEmbed()
-                    .addField('» السيرفر :', `${message.guild.name}`)
-                    .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-                    .addField(' » الرسالة : ', args)
-                    .setColor('#ff0000')
-                    // m.send(`[${m}]`);
-                    m.send(`${m}`,{embed: bc});
-                });
-            }
-            }
+        }
             if(message.content.startsWith(prefix + 'animal')) {
                 var cats = ["https://i.ytimg.com/vi/SfLV8hD7zX4/maxresdefault.jpg","http://www.dogbazar.org/wp-content/uploads/2014/09/british-bull-dog-puppies.jpg","http://cdn2-www.dogtime.com/assets/uploads/gallery/german-shepherd-dog-breed-pictures/standing-7.jpg","http://cdn.akc.org/Marketplace/Breeds/German_Shepherd_Dog_SERP.jpg","https://animalso.com/wp-content/uploads/2016/12/black-german-shepherd_2.jpg","https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpg","https://www.petfinder.com/wp-content/uploads/2012/11/101438745-cat-conjunctivitis-causes.jpg","http://www.i-love-cats.com/images/2015/04/12/cat-wallpaper-38.jpg","https://www.aspca.org/sites/default/files/cat-care_urine-marking_main-image.jpg","https://s-media-cache-ak0.pinimg.com/originals/f0/3b/76/f03b7614dfadbbe4c2e8f88b69d12e04.jpg","http://www.rd.com/wp-content/uploads/sites/2/2016/04/15-cat-wants-to-tell-you-attention.jpg","https://www.thelocal.de/userdata/images/article/fa6fd5014ccbd8f4392f716473ab6ff354f871505d9128820bbb0461cce1d645.jpg","https://www.adelaidezoo.com.au/wp-content/uploads/sites/2/animals/GiantPanda3Slider.jpg","http://imagem.band.com.br/f_230168.jpg"]
                    var cat = new Discord.RichEmbed()
